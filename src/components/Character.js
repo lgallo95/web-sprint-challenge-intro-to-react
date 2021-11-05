e36c8d13b0c1defaa0394d01e73aeda27cc8ebc2
lgallo95 <lgallo2005@gmail.com>
2021-11-05 16:58:49 -0700

    Tried the button

--- src/components/Character.js
+++ src/components/Character.js
@@ -11,18 +11,25 @@ const MainStyle2 = styled.div`
 
 const Character = (props) => {
     const [open, setOpen] = useState(false);
-
-    const toggleInfo = () => {
-        <p> {props.person.name} </p>
-        setOpen(open);
         
-        };
+    function myFunction() {
+            var x = document.getElementById("infoP");
+            if (x.style.display === "none") {
+              x.style.display = "block";
+            } else {
+              x.style.display = "none";
+            }
+          }
     
     return (
         <div>
             <MainStyle2>
             <h3> {props.person.name} </h3>
             <button onclick="toggleInfo()"> More Info </button>
+            <p id="infoP">{props.person.height}</p>
+            <p id="infoP">{props.person.mass}</p>
+            <p id="infoP">{props.person.hair_color}</p>
+            <p id="infoP">{props.person.skin_color}</p>
             </MainStyle2>
         </div>
     )
